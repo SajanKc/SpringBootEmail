35e5bb667424016ac45bc501ffd5c3e0389ab065
Sajan Kc <sajan1996kc@gmail.com>
2021-02-01 18:26:32 +0545

    post mapping added

--- src/main/java/com/kcsajan/main/EmailController.java
+++ src/main/java/com/kcsajan/main/EmailController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +25,15 @@ public class EmailController {
 		message.setText("Hello there! This is a demo text...");
 		javaMailSender.send(message);
 		return "Email sent successfully to " + emailReceiver;
+	}
+
+	@PostMapping("/send")
+	public String sendCustomEmail(@RequestBody Email email) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo(email.getTo());
+		message.setSubject(email.getSubject());
+		message.setText(email.getText());
+		javaMailSender.send(message);
+		return "Email send successfully to " + email.getTo();
 	}
 }
